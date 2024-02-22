594eeafbf29f54aa2d5ab3fcf7962951edeed36a
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2024-02-21 17:05:04 -0700

    Smoother Rectangles

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -38,9 +38,10 @@ void au::Rectangle::update(float time, std::vector<float> volume) {
   if (volume[_band] == 0) {
     _rect.setSize(sf::Vector2f(_rect.getSize().x, _rect.getSize().y * .98f));
   } else {
-    _rect.setSize(
-        sf::Vector2f(_rect.getSize().x,
-                     (.2 * _maxHeight) + (volume[_band] * (1.2 * _maxHeight))));
+    float rate = .2f;
+    float newHeight = (0 * _maxHeight) + (rate * (volume[_band] * _maxHeight) +
+                                          (1 - rate) * _rect.getSize().y);
+    _rect.setSize(sf::Vector2f(_rect.getSize().x, newHeight));
   }
 
   _rect.setOrigin(sf::Vector2f(0, _rect.getSize().y));
